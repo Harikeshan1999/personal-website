c66477160ff106a23eef142973b178d590274d5f
unknown <jesumathu11@gmail.com>
2025-02-17 16:16:30 +0530

    mathu updatedd

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-website/',  // Ensure this matches your repo name
+  base: '/personal-website/', // Replace with your GitHub repo name
   build: {
-    outDir: 'build',  // Ensures correct build directory
+    outDir: 'build',
   },
 });
